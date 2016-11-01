5aea195c1120a0b098ef9b2a9b21544a3998e23c
beardypig <beardypig@users.noreply.github.com>
2016-11-01 13:10:11 +0000

    prepare for version 0.0.2 release

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+"""
+   Copyright 2016 beardypig
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
 
 from os.path import abspath, dirname, join
 from setuptools import setup
@@ -14,7 +29,7 @@ srcdir = join(dirname(abspath(__file__)), "src/")
 sys_path.insert(0, srcdir)
 
 setup(name="pymp4",
-      version="0.0.1",
+      version="0.0.2",
       description="A Python parser for MP4 boxes",
       url="https://github.com/beardypig/pymp4",
       author="beardypig",
