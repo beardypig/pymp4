d27c7bc31450124f663afa868a38808491e3ef78
beardypig <beardypig@users.noreply.github.com>
2016-11-09 10:00:48 +0000

    update version number to 1.0.2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ srcdir = join(dirname(abspath(__file__)), "src/")
 sys_path.insert(0, srcdir)
 
 setup(name="pymp4",
-      version="1.0.1",
+      version="1.0.2",
       description="A Python parser for MP4 boxes",
       url="https://github.com/beardypig/pymp4",
       author="beardypig",
