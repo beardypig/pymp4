0e5e505630dbb952b0c3860455f3280aa9d30d5c
beardypig <beardypig@protonmail.com>
2021-02-04 03:42:17 +0100

    some basic support for hvcC

--- src/pymp4/parser.py
+++ src/pymp4/parser.py
@@ -333,6 +333,48 @@ class MaskedInteger(Adapter):
         return obj & 0x1F
 
 
+AAVC = Struct(
+    "version" / Const(Int8ub, 1),
+    "profile" / Int8ub,
+    "compatibility" / Int8ub,
+    "level" / Int8ub,
+    EmbeddedBitStruct(
+        Padding(6, pattern=b'\x01'),
+        "nal_unit_length_field" / Default(BitsInteger(2), 3),
+    ),
+    "sps" / Default(PrefixedArray(MaskedInteger(Int8ub), PascalString(Int16ub)), []),
+    "pps" / Default(PrefixedArray(Int8ub, PascalString(Int16ub)), [])
+)
+
+HVCC = Struct(
+    EmbeddedBitStruct(
+        "version" / Const(BitsInteger(8), 1),
+        "profile_space" / BitsInteger(2),
+        "general_tier_flag" / BitsInteger(1),
+        "general_profile" / BitsInteger(5),
+        "general_profile_compatibility_flags" / BitsInteger(32),
+        "general_constraint_indicator_flags" / BitsInteger(48),
+        "general_level" / BitsInteger(8),
+        Padding(4, pattern=b'\xff'),
+        "min_spatial_segmentation" / BitsInteger(12),
+        Padding(6, pattern=b'\xff'),
+        "parallelism_type" / BitsInteger(2),
+        Padding(6, pattern=b'\xff'),
+        "chroma_format" / BitsInteger(2),
+        Padding(5, pattern=b'\xff'),
+        "luma_bit_depth" / BitsInteger(3),
+        Padding(5, pattern=b'\xff'),
+        "chroma_bit_depth" / BitsInteger(3),
+        "average_frame_rate" / BitsInteger(16),
+        "constant_frame_rate" / BitsInteger(2),
+        "num_temporal_layers" / BitsInteger(3),
+        "temporal_id_nested" / BitsInteger(1),
+        "nalu_length_size" / BitsInteger(2),
+    ),
+    # TODO: parse NALUs
+    "raw_bytes" / GreedyBytes
+)
+
 AVC1SampleEntryBox = Struct(
     "version" / Default(Int16ub, 0),
     "revision" / Const(Int16ub, 0),
@@ -351,52 +393,13 @@ AVC1SampleEntryBox = Struct(
     "depth" / Default(Int16ub, 24),
     "color_table_id" / Default(Int16sb, -1),
     "avc_data" / PrefixedIncludingSize(Int32ub, Struct(
-        "type" / Const(b"avcC"),
-        "version" / Const(Int8ub, 1),
-        "profile" / Int8ub,
-        "compatibility" / Int8ub,
-        "level" / Int8ub,
-        EmbeddedBitStruct(
-            Padding(6, pattern=b'\x01'),
-            "nal_unit_length_field" / Default(BitsInteger(2), 3),
-        ),
-        "sps" / Default(PrefixedArray(MaskedInteger(Int8ub), PascalString(Int16ub)), []),
-        "pps" / Default(PrefixedArray(Int8ub, PascalString(Int16ub)), [])
-    ))
-)
-
-EncryptedAVC1SampleEntryBox = Struct(
-    "version" / Default(Int16ub, 0),
-    "revision" / Const(Int16ub, 0),
-    "vendor" / Default(String(4, padchar=b" "), b"brdy"),
-    "temporal_quality" / Default(Int32ub, 0),
-    "spatial_quality" / Default(Int32ub, 0),
-    "width" / Int16ub,
-    "height" / Int16ub,
-    "horizontal_resolution" / Default(Int16ub, 72),  # TODO: actually a fixed point decimal
-    Padding(2),
-    "vertical_resolution" / Default(Int16ub, 72),  # TODO: actually a fixed point decimal
-    Padding(2),
-    "data_size" / Const(Int32ub, 0),
-    "frame_count" / Default(Int16ub, 1),
-    "compressor_name" / Default(String(32, padchar=b" "), ""),
-    "depth" / Default(Int16ub, 24),
-    "color_table_id" / Default(Int16sb, -1),
-    "avc_data" / PrefixedIncludingSize(Int32ub, Struct(
-        "type" / Const(b"avcC"),
-        "version" / Const(Int8ub, 1),
-        "profile" / Int8ub,
-        "compatibility" / Int8ub,
-        "level" / Int8ub,
-        EmbeddedBitStruct(
-            Padding(6, pattern=b'\x01'),
-            "nal_unit_length_field" / Default(BitsInteger(2), 3),
-        ),
-        "sps" / Default(PrefixedArray(MaskedInteger(Int8ub), PascalString(Int16ub)), []),
-        "pps" / Default(PrefixedArray(Int8ub, PascalString(Int16ub)), [])
+    "type" / String(4, padchar=b" ", paddir="right"),
+        Embedded(Switch(this.type, {
+            b"avcC": AAVC,
+            b"hvcC": HVCC,
+        }, Struct("data" / GreedyBytes)))
     )),
-    "sample_info" / LazyBound(lambda _: PrefixedIncludingSize(Int32ub, ProtectionSchemeInformationBox))
-
+    "sample_info" / LazyBound(lambda _: GreedyRange(Box))
 )
 
 SampleEntryBox = PrefixedIncludingSize(Int32ub, Struct(
@@ -408,7 +411,7 @@ SampleEntryBox = PrefixedIncludingSize(Int32ub, Struct(
         b"mp4a": MP4ASampleEntryBox,
         b"enca": MP4ASampleEntryBox,
         b"avc1": AVC1SampleEntryBox,
-        b"encv": EncryptedAVC1SampleEntryBox
+        b"encv": AVC1SampleEntryBox
     }, Struct("data" / GreedyBytes)))
 ))
 
